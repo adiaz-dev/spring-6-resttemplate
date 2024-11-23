f903fd9065b656efe85a6dd0b2bdca0cc931abb8
Augusto Diaz <augustodiaz@fico.com>
2024-11-23 11:43:50 -0600

    Code after section 193

---
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring6resttemplate.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerClientImplTest {
+
+  @Autowired
+  private BeerClient beerClient;
+
+  @Test
+  void listBeers() {
+    beerClient.listBeers();
+
+  }
+}
